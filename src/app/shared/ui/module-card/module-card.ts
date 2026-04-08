46add72292dfdec08ab7d0a109e7fc4caaa7fbd9
Novikov <novukov.b@v2grp.ru>
2026-04-08 16:59:45 +0500

    components + header(50%)

---
+++ src/app/shared/ui/module-card/module-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-module-card',
+  imports: [],
+  templateUrl: './module-card.html',
+  styleUrl: './module-card.css',
+})
+export class ModuleCard {
+
+}
